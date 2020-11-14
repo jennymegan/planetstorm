094765e11f70c7a57ddd05ec257028d05a2526fd
Jenny Allton-Twist <jennyalltontwist@gmail.com>
2020-11-14 14:34:29 +0000

    timer installed, asteroid movement added

--- script.js
+++ script.js
@@ -1,83 +1,111 @@
+let timer
+let timerRunning = false
 
-function runTimer () {
-    let sec = 30
-    setInterval(function() {
-        document.querySelector("#timer").innerHTML = sec
-        sec--
-        if (sec === 0) {
-            clearInterval()
+function startTimer(duration, element) {
+    timerRunning = true
+    let timeLeft = duration
+    timer = setInterval(() => {
+        let seconds = parseInt(timeLeft % 60, 10)
+        seconds = (seconds < 10) ? ("0" + seconds) : seconds
+
+        element.textContent = seconds
+        --timeLeft
+        if (timeLeft < 0) {
+            stopGame()
+            document.querySelector("#gameLosePopup").style.display = 'block'
         }
     }, 1000)
 }
 
-function runGame () {
-    document.querySelector('.rotateNeptune').style.cursor = 'pointer'
-    document.querySelector('.rotateNeptune').addEventListener('click', () => {
-        document.querySelector('.rotateNeptune').style.animationPlayState="paused"
-        document.querySelector('.rotateUranus').style.cursor = 'pointer'
-        document.querySelector('.rotateUranus').addEventListener('click', () => {
-            document.querySelector('.rotateUranus').style.animationPlayState="paused"
-            document.querySelector('.rotateSaturn').style.cursor = 'pointer'
-            document.querySelector('.rotateSaturn').addEventListener('click', () => {
-                document.querySelector('.rotateSaturn').style.animationPlayState="paused"
-                document.querySelector('.rotateJupiter').style.cursor = 'pointer'
-                document.querySelector('.rotateJupiter').addEventListener('click', () => {
-                    document.querySelector('.rotateJupiter').style.animationPlayState="paused"
-                    document.querySelector('.rotateMars').style.cursor = 'pointer'
-                    document.querySelector('.rotateMars').addEventListener('click', () => {
-                        document.querySelector('.rotateMars').style.animationPlayState="paused"
-                        document.querySelector('.rotateEarth').style.cursor = 'pointer'
-                        document.querySelector('.rotateEarth').addEventListener('click', () => {
-                            document.querySelector('.rotateEarth').style.animationPlayState="paused"
-                            document.querySelector('.rotateVenus').style.cursor = 'pointer'
-                            document.querySelector('.rotateVenus').addEventListener('click', () => {
-                                document.querySelector('.rotateVenus').style.animationPlayState="paused"
-                                document.querySelector('.rotateMercury').style.cursor = 'pointer'
-                                document.querySelector('.rotateMercury').addEventListener('click', () => {
-                                    document.querySelector('.rotateMercury').style.animationPlayState="paused"
-                                    document.querySelector("#gameWinPopup").style.display = 'block'
-                                    clearInterval(runTimer)
-                                })
-                            })
-                        })
-                    })
-                })
-            })
-        })
+
+function resetGame() {
+    document.querySelectorAll('.planets').forEach((planet, i) => {
+        planet.classList.remove('stopped')
+        planet.style.animationPlayState="running"
+        let num = planet.dataset.num
+        if ( planet.dataset.num !== "1") {
+            planet.style.cursor= 'initial'
+        }
+        asteroidMove.play();
+        if (timerRunning === false) {
+            startTimer(29, document.querySelector('#timer'));
+        }
     })
 }
 
-
-
-
+function stopGame() {
+    document.querySelectorAll('.planets').forEach((planet, i) => {
+        planet.style.animationPlayState="paused"
+        asteroidMove.pause();
+        clearInterval(timer);
+        timerRunning = false;
+    })
+}
 
 document.querySelector('.startButton').addEventListener('click', () => {
     document.querySelector('#splash').style.display = 'none';
     document.querySelector('#game').style.display = 'flex';
-    //runTimer()
-    runGame()
+    document.querySelector('#timer').style.display = 'initial';
+    asteroidMove.play()
+    if (timerRunning === false) {
+        startTimer(29, document.querySelector('#timer'));
+    }
 })
 
 document.querySelector('#closeWinModal').addEventListener('click', () => {
     //runTimer()
     document.querySelector("#gameWinPopup").style.display = "none"
-    document.querySelectorAll('.planets').forEach((planet) => {
-        //planet. remove event listeners
-        planet.style.animationPlayState="running"
-        planet.style.cursor = "default"
-    })
-    runGame()
+    resetGame()
+})
+
+document.querySelector('#closeLoseModal').addEventListener('click', () => {
+    //runTimer()
+    document.querySelector("#gameLosePopup").style.display = "none"
+    resetGame()
 })
 
 
 // to do
 // timer, asteroids, styling, remove event listeners
-// find the next planet and apply the event listener
-//add event listeners to all planets at the start - inside event listener check if previosu one has been clicked and if not, do nothing
-//give each a number - this one is 5, has 4 stopped ? Or add a class "stopped" when they stop - check whether they have that class.
 
-document.querySelectorAll('.planets').forEach((planet) => {
-    if
-    planet.addEventListener('click')
+document.querySelectorAll('.planets').forEach((planet, i) => {
+    planet.addEventListener('click', () => {
+        let num = planet.dataset.num
+        if (document.querySelector('[data-num="' + (num-1) + '"]').classList.contains('stopped') &&
+            planet.dataset.num !== "8" )
+        {
+            planet.style.animationPlayState="paused"
+            planet.classList.add('stopped')
+            let next = parseInt(num)+1
+            document.querySelector('[data-num="' + next + '"]').style.cursor = 'pointer'
+        }
+
+        if(document.querySelector('[data-num="' + (num-1) + '"]').classList.contains('stopped') &&
+            planet.dataset.num === "8")
+        {
+            planet.style.animationPlayState="paused"
+            planet.classList.add('stopped')
+            document.querySelector("#gameWinPopup").style.display = 'block'
+            stopGame()
+        }
+    })
 })
-element.classList.contains('stopped')
+
+
+const asteroid = document.querySelector('.asteroid')
+const asteroidMove = asteroid.animate([
+    {transform: 'translate(-1200px,-1200px)'},
+    {transform: 'translate(1500px,3020px)'},
+    {transform: 'translate(-30px,-30px)'},
+    {transform: 'translate(1900px,500px)'},
+    {transform: 'translate(900px,1220px)'}
+], {
+    duration: 20000,
+    iterations: Infinity
+});
+asteroidMove.pause();
+
+asteroid.addEventListener('mouseover', ()=> {
+    stopGame()
+    document.querySelector("#gameLosePopup").style.display = 'block'
+})
